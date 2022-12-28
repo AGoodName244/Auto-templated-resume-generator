ca02caba27489b35c85068c318742f18de3775c6
Lyuyongkang Yuan <lyuyongkang.yuan@outlook.com>
2022-12-27 23:12:36 -0500

    Initial commit

---
+++ auto_generator/model.py
@@ -0,0 +1,10 @@
+"""Auto generator Model File."""
+import sqlite3
+import flask
+
+def dict_factory(cursor, row):
+    """Convert database row objects to a dictionary keyed on column name.
+    This is useful for building dictionaries which are then used to render a
+    template.  Note that this would be inefficient for large queries.
+    """
+    return {col[0]: row[idx] for idx, col in enumerate(cursor.description)}
